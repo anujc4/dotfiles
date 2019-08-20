811dc03d3edac51bee7ad535db7ef15d9c42260a
Anuj Chandra <anujchandra@Anuj.local>
2019-08-20 13:31:49 +0530

    Add yabai rules for Jetbrains IDEs

---
+++ yabai/jetbrains.sh
@@ -0,0 +1,7 @@
+# check if there is exactly one window belonging to the process of the window that was just created
+if yabai -m query --windows \
+    | jq -er 'map(select(.id == env.YABAI_WINDOW_ID).pid)[0] as $pid | map(select(.pid == $pid)) | length == 1' >  /dev/null
+then
+    # unfloat the window
+    yabai -m window "${YABAI_WINDOW_ID}"  --toggle float
+fi
