be97d74a505bd24f25dd6c4013fd886943e371b2
anujc4 <anujc4@gmail.com>
2019-12-25 03:20:38 +0530

    Add setup script for regolith

---
+++ regolith/setup.sh
@@ -0,0 +1,5 @@
+# Modify these to point to correct directory
+ln -sf $(echo $DOT_SOURCE)/regolith/Xresources/.Xresources-regolith ~/.Xresources-regolith
+ln -sf $(echo $DOT_SOURCE)/regolith/Xresources/Xresources.d ~/.Xresources.d
+ln -sf $(echo $DOT_SOURCE)/regolith/i3/config /home/anuj/.config/regolith/i3/config
+ln -sf $(echo $DOT_SOURCE)/regolith/compton /home/anuj/.config/compton
