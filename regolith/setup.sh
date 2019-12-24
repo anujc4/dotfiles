30fd9c088ed97649a2b49dfc6c65f46db8e3301c
anujc4 <anujc4@gmail.com>
2019-12-25 04:00:43 +0530

    Add regolith installation steps in setup.sh

--- regolith/setup.sh
+++ regolith/setup.sh
@@ -1,3 +1,13 @@
+NC='\033[0m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+
+printf "${GREEN}Installing regolith linux if not already installed.\n${NC}"
+sudo add-apt-repository -y ppa:kgilmer/regolith-stable
+sudo apt install regolith-desktop
+
+printf "${YELLOW}If you are on Ubuntu 19.10, check out https://github.com/regolith-linux/regolith-desktop/wiki/HowTo:-Upgrade-to-Ubuntu-19.10-(Eoan-Ermine).\n${NC}"
+
 # Modify these to point to correct directory
 ln -sf $(echo $DOT_SOURCE)/regolith/Xresources/.Xresources-regolith ~/.Xresources-regolith
 ln -sf $(echo $DOT_SOURCE)/regolith/Xresources/Xresources.d ~/.Xresources.d
