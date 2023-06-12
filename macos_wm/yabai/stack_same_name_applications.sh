d667ba73055b5683293f9d016a4f6f74d38a57bd
anujc4 <anujc4@gmail.com>
2023-06-12 12:39:47 +0100

    * Add common alias * Add ability to stack a new application on top of existing application with the same name if exists

--- macos_wm/yabai/stack_same_name_applications.sh
+++ macos_wm/yabai/stack_same_name_applications.sh
@@ -6,10 +6,21 @@ current_window=$(yabai -m query --windows --window "$YABAI_WINDOW_ID")
 current_window_name=$(echo "$current_window" | jq .app)
 current_window_id=$(echo "$current_window" | jq .id)
 
-prev_window=$(yabai -m query --windows --window recent)
-prev_window_name=$(echo "$prev_window" | jq .app)
-prev_window_id=$(echo "$prev_window" | jq .id)
+# look for other windows in the same space
+other_windows=$(yabai -m query --windows --space)
 
-if [[ $current_window_name =~ $whitelisted_applications ]] && [[ $current_window_name = "$prev_window_name" ]]; then
-  yabai -m window "$prev_window_id" --stack "$current_window_id"
-fi
+# Iterate over all other apps in the same space to see if there's any other app
+# already open in the space
+echo "$other_windows" | jq -c '.[]' | while read -r i; do
+    window_name=$(echo "$i" | jq .app)
+    window_id=$(echo "$i" | jq .id)
+
+    if [[ $window_id = "$current_window_id" ]]; then
+      continue
+    fi
+
+    if [[ $current_window_name =~ $whitelisted_applications ]] && [[ $current_window_name = "$window_name" ]]; then
+      yabai -m window "$window_id" --stack "$current_window_id"
+      break
+    fi
+done
