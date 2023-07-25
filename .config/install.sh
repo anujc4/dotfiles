07e2a4ef45cedbd280fa2b093d1b9dc6ae873e94
anujc4 <anujc4@gmail.com>
2023-07-25 12:42:06 +0100

    incremental

---
+++ .config/install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ln -sf "$DOT_SOURCE/.config/spaceship.zsh" "$HOME/.config/spaceship.zsh"
---
+++ .config/spaceship.zsh
@@ -0,0 +1,12 @@
+# Display time
+SPACESHIP_TIME_SHOW=true
+
+# Display username always
+# SPACESHIP_USER_SHOW=always
+
+# Do not truncate path in repos
+# SPACESHIP_DIR_TRUNC_REPO=false
+
+SPACESHIP_PROMPT_SEPARATE_LINE=false
+
+SPACESHIP_PROMPT_ADD_NEWLINE=false
