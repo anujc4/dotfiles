ea016e129a129d70183ec662374bcdb8e5968f52
anujc4 <anujc4@gmail.com>
2024-02-10 16:04:58 +0000

    add config for borders

--- macos_wm/install.sh
+++ macos_wm/install.sh
@@ -1,27 +1,23 @@
 #!/usr/bin/env bash
 
-if ! command -v brew &> /dev/null
-then
+if ! command -v brew &>/dev/null; then
     echo "missing brew. installing it now."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-if ! command -v yabai &> /dev/null
-then
+if ! command -v yabai &>/dev/null; then
     echo "missing yabai. installing it now."
     brew tap koekeishiya/formulae
     brew install yabai
 fi
 
-if ! command -v skhd &> /dev/null
-then
+if ! command -v skhd &>/dev/null; then
     echo "missing skhd. installing it now."
     brew tap koekeishiya/formulae
     brew install skhd
 fi
 
-if ! command -v sketchybar &> /dev/null
-then
+if ! command -v sketchybar &>/dev/null; then
     echo "missing sketchybar. installing it now."
     brew tap FelixKratz/formulae
     brew install sketchybar
@@ -32,19 +28,24 @@ then
     brew install --cask sf-symbols
 fi
 
-if ! command -v jq &> /dev/null
-then
+if ! command -v borders &>/dev/null; then
+    echo "missing borders. installing it now."
+    brew install borders
+fi
+
+if ! command -v jq &>/dev/null; then
     echo "missing jq. installing it now."
     brew install jq
 fi
 
 if [[ -z "${DOT_SOURCE}" ]]; then
-  echo "DOT_SOURCE variable is not available. Have you set up zsh?"
-  exit 1
+    echo "DOT_SOURCE variable is not available. Have you set up zsh?"
+    exit 1
 fi
 
 ln -sf "$DOT_SOURCE/macos_wm/common" "$HOME/.config/common_wm"
 ln -sf "$DOT_SOURCE/macos_wm/sketchybar" "$HOME/.config"
+ln -sf "$DOT_SOURCE/macos_wm/borders" "$HOME/.config"
 ln -sf "$DOT_SOURCE/macos_wm/yabai" "$HOME/.config"
 ln -sf "$DOT_SOURCE/macos_wm/skhd" "$HOME/.config"
 
